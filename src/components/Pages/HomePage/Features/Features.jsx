1cc2340bf2e418f8ca144478c5eda98411823728
Vlad Korochanskiy <mginster234942@gmail.com>
2025-05-07 12:32:10 +0300

    asdsadфф

--- src/components/Pages/HomePage/Features/Features.jsx
+++ src/components/Pages/HomePage/Features/Features.jsx
@@ -4,13 +4,13 @@ import s from "./Features.module.scss";
 const Features = () => {
   const featuresBlock = [
     {
-      img: "/images/circleicon1.png",
+      img: "/images/circleicon2.png",
       title: "Premium Quality",
       subtitle:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu",
     },
     {
-      img: "/images/circleicon2.png",
+      img: "/images/circleicon1.png",
       title: "Seasonal Vegetables",
       subtitle:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu",
